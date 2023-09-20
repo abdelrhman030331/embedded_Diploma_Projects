92ac7cf5ab54a76ae2e3bf3c36c77a3540025103
abdelrhman magdy <abdo01067128258@gmail.com>
2023-09-20 06:42:49 +0300

    I put assignment from lecture 3

--- basic.C__ass-1/sequence_pro/src/sequence_pro.c
+++ basic.C__ass-1/sequence_pro/src/sequence_pro.c
@@ -1,17 +1,26 @@
 /*
  ============================================================================
- Name        : ex7.c
+ Name        : sequence_pro.c
  Author      : Abdelrhman Magdy
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
  ============================================================================
  */
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int main() {
+	int a ;  // define variables
+	printf("Enter value of matrixs:"); /* prints a message for user  */
+	fflush(stdout); fflush(stdin);
+	scanf("%d", &a);
+	for (int i=0;i<=a;i++)
+	{
+		for (int x=0;x<=a;x++)
+		{
+			if (x>=i) printf("%d ",x);
+		}
+		printf("\n");
+	}
 }
